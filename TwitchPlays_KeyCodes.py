5d45987a21ec19ccb44cc2dd1e7b43f4722c1f2f
BloopVerified <53751791+BloopVerified@users.noreply.github.com>
2023-11-25 19:13:24 -0600

    Update TwitchPlays_KeyCodes.py

--- TwitchPlays_KeyCodes.py
+++ TwitchPlays_KeyCodes.py
@@ -1,3 +1,9 @@
 # DougDoug Note: 
 # This code contains key codes plus functions to press keys on Windows
 # You should not need to modify anything in this file, just use as is.
+# 
+#
+#
+#
+# Fork Note
+# Removed by fork creator
