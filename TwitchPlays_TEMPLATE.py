03ebd6b5e6a788832eb94e0814bebbfde9caafa7
Bloop <53751791+BloopVerified@users.noreply.github.com>
2023-12-09 23:40:26 -0600

    Update TwitchPlays_TEMPLATE.py
    Vertical Monitor Update

--- TwitchPlays_TEMPLATE.py
+++ TwitchPlays_TEMPLATE.py
@@ -1,18 +1,18 @@
 import concurrent.futures
-import random
-import TwitchPlays_Connection
+import ctypes
 import threading as threading
-import keyboard
 import time
-import tkinter as Tk
-from tkinter import *
-import mttkinter as mtTkinter
-from mttkinter import *
-import pydirectinput
-import pyautogui
-import mouse
 from functools import partial
-import sys
+from tkinter import *
+import keyboard
+import mouse
+import mttkinter as mtTkinter
+import pyautogui
+import pydirectinput
+from mttkinter import *
+from screeninfo import get_monitors, Monitor
+import TwitchPlays_Connection
+
 
 class ScreenManage():
     def __init__(self):
@@ -38,12 +38,17 @@ class ScreenManage():
         screenRes.iconbitmap("icon.ico")
         screenRes.geometry("300x300+700+300")
         screenRes.config(background="white")
-        screenRes.minsize(300, 300)
-        screenRes.maxsize(300, 300)
+        screenRes.minsize(300, 400)
+        screenRes.maxsize(300, 400)
         youtubePhoto = PhotoImage(file="youtube.png")
         youtubeResize = youtubePhoto.subsample(3, 3)
         twitchPhoto = PhotoImage(file="twitch.png")
         twitchResize = twitchPhoto.subsample(3, 3)
+        separatorText = Label(screenRes, text="- or -",
+                        bg="white",
+                        fg="black",
+                        font=("Arial", 10))
+        separatorText.place(x=135, y=100)
         ScreenXStart = Label(screenRes, text="Enter Screen X Start Variable",
                         bg="white",
                         fg="black",
@@ -65,20 +70,137 @@ class ScreenManage():
                         font=("Arial", 10))
         ScreenYStartEntry = Entry(screenRes, width=25, textvariable=self.startY)
         ScreenYEndEntry = Entry(screenRes, width=25, textvariable=self.endY)
-        ScreenXStart.place(x=10, y=25)
-        ScreenXStartEntry.place(x=10, y=50)
-        ScreenXEnd.place(x=10, y=75)
-        ScreenXEndEntry.place(x=10, y=100)
-        ScreenYStart.place(x=10, y=125)
-        ScreenYStartEntry.place(x=10, y=150)
-        ScreenYEnd.place(x=10, y=175)
-        ScreenYEndEntry.place(x=10, y=200)
+        monitorOneFunc = partial(ScreenManage.monitorDecisionOne, self, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry)
+        monitorTwoFunc = partial(ScreenManage.monitorDecisionTwo, self, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry)
+        monitorThreeFunc = partial(ScreenManage.monitorDecisionThree, self, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry)
+        monitorOne = Button(screenRes, text="Screen 1",height = 2, width = 6, command = monitorOneFunc)
+        monitorTwo = Button(screenRes, text="Screen 2",height = 2, width = 6, command = monitorTwoFunc)
+        monitorThree = Button(screenRes, text="Screen 3",height = 2, width = 6, command = monitorThreeFunc)
+        monitorOne.place(x=25, y=25)
+        monitorTwo.place(x=125, y=25)
+        monitorThree.place(x=225, y=25)
+        ScreenXStart.place(x=25, y=150)
+        ScreenXStartEntry.place(x=25, y=175)
+        ScreenXEnd.place(x=25, y=200)
+        ScreenXEndEntry.place(x=25, y=225)
+        ScreenYStart.place(x=25, y=250)
+        ScreenYStartEntry.place(x=25, y=275)
+        ScreenYEnd.place(x=25, y=300)
+        ScreenYEndEntry.place(x=25, y=325)
         screenLimitFunc = partial(ScreenManage.ScreenGet, self,screenRes, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry)
         SubmitScreenButton = Button(screenRes, height = 1, width = 5, text="Submit", font = ("Arial", 12), command= screenLimitFunc)
-        SubmitScreenButton.place(x=125, y=230)
+        SubmitScreenButton.place(x=125, y=360)
         screenRes.after(1, self.updateScreenEntry, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry)
         closeRes = partial(ScreenManage.screenRes_close_window, self, screenRes)
         screenRes.protocol("WM_DELETE_WINDOW", closeRes)
+
+    def monitorDecisionOne(self, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry):
+        try:
+            testing = [''] *3
+            z = 0
+            for m in get_monitors():
+                testing[z] = str(m)
+                z+= 1
+            monitorOne = testing[0].split()
+            monitorHeight = monitorOne[3]
+            monitorHeight = monitorHeight.replace('height=','')
+            monitorHeight = monitorHeight.replace(',','')
+            monitorWidth = monitorOne[2]
+            monitorWidth = monitorWidth.replace('width=','')
+            monitorWidth = monitorWidth.replace(',','')
+            user32 = ctypes.windll.user32
+            user32.SetProcessDPIAware()
+
+            startX = 0
+            endX = monitorWidth
+            startY = 0
+            endY = monitorHeight
+        except Exception as e:
+            startX = 0
+            endX = 0
+            startY = 0
+            endY = 0
+
+        ScreenXStartEntry.delete(0, END)
+        ScreenXStartEntry.insert(END, startX)
+        ScreenXEndEntry.delete(0, END)
+        ScreenXEndEntry.insert(END, endX)
+        ScreenYStartEntry.delete(0, END)
+        ScreenYStartEntry.insert(END, startY)
+        ScreenYEndEntry.delete(0, END)
+        ScreenYEndEntry.insert(END, endY)
+
+    def monitorDecisionTwo(self, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry):
+        try:
+            testing = [''] *3
+            z = 0
+            for m in get_monitors():
+                testing[z] = str(m)
+                z+= 1
+            monitorOne = testing[1].split()
+            monitorHeight = monitorOne[3]
+            monitorHeight = monitorHeight.replace('height=','')
+            monitorHeight = monitorHeight.replace(',','')
+            monitorWidth = monitorOne[2]
+            monitorWidth = monitorWidth.replace('width=','')
+            monitorWidth = monitorWidth.replace(',','')
+            user32 = ctypes.windll.user32
+            user32.SetProcessDPIAware()
+
+            startX = int(monitorWidth)
+            endX = int(monitorWidth) * 2
+            startY = 0
+            endY = monitorHeight
+        except Exception as e:
+            startX = 0
+            endX = 0
+            startY = 0
+            endY = 0
+
+        ScreenXStartEntry.delete(0, END)
+        ScreenXStartEntry.insert(END, startX)
+        ScreenXEndEntry.delete(0, END)
+        ScreenXEndEntry.insert(END, endX)
+        ScreenYStartEntry.delete(0, END)
+        ScreenYStartEntry.insert(END, startY)
+        ScreenYEndEntry.delete(0, END)
+        ScreenYEndEntry.insert(END, endY)
+
+    def monitorDecisionThree(self, ScreenXStartEntry, ScreenXEndEntry, ScreenYStartEntry, ScreenYEndEntry):
+        try:
+            testing = [''] *3
+            z = 0
+            for m in get_monitors():
+                testing[z] = str(m)
+                z+= 1
+            monitorOne = testing[2].split()
+            monitorHeight = monitorOne[3]
+            monitorHeight = monitorHeight.replace('height=','')
+            monitorHeight = monitorHeight.replace(',','')
+            monitorWidth = monitorOne[2]
+            monitorWidth = monitorWidth.replace('width=','')
+            monitorWidth = monitorWidth.replace(',','')
+            user32 = ctypes.windll.user32
+            user32.SetProcessDPIAware()
+
+            startX = int(monitorWidth) * 2
+            endX = int(monitorWidth) * 3
+            startY = 0
+            endY = monitorHeight
+        except Exception as e:
+            startX = 0
+            endX = 0
+            startY = 0
+            endY = 0
+        ScreenXStartEntry.delete(0, END)
+        ScreenXStartEntry.insert(END, startX)
+        ScreenXEndEntry.delete(0, END)
+        ScreenXEndEntry.insert(END, endX)
+        ScreenYStartEntry.delete(0, END)
+        ScreenYStartEntry.insert(END, startY)
+        ScreenYEndEntry.delete(0, END)
+        ScreenYEndEntry.insert(END, endY)
+
 
     def screenRes_close_window(self, screenRes):
         screenRes.destroy()
